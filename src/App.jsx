0bda0c4d8bf2be836ec313d27458cc365edfed3c
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-07-09 15:49:10 +0530

    App: Use Array.map to add images + style it

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import Card from './components/Card.jsx'
+import data from "./data"
 
 function App() {
+
+  let Cards = data.map((i) => 
+    <Card img={i.coverImg} 
+          rating={i.stats.rating} 
+          reviewCount={i.stats.reviewCount} 
+          location={i.location} 
+          title={i.title} 
+          price={i.price}/>);
+
   return (
   <div>
     <Navbar/>
     <Hero/>
+    <div className="cards--list">{Cards}</div>
+    {/* <Card 
+      img="katie-zaferes.png"
+      rating="5.0"
+      reviewCount={6}
+      location="USA"
+      title="Life Lessons with Katie Zaferes"
+      price={136}
+    /> */}
+
     </div>
   )
 }
